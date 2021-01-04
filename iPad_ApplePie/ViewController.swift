585aa2033371355f4cabd899e97fc1b16e1ef854
imjeongwoo <limjeong1219@gmail.com>
2021-01-04 21:07:45 +0900

    Initial Commit

---
+++ iPad_ApplePie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iPad_ApplePie
+//
+//  Created by 임정우 on 2021/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
